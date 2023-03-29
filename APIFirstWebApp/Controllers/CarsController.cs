03bbc321c8853985f7d7175ed32496c7683c5fec
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-03-29 16:34:01 -0300

    Add project files.

---
+++ APIFirstWebApp/Controllers/CarsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIFirstWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        // GET: api/<CarsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Carro1", "Carro2" };
+        }
+
+        // GET api/<CarsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CarsController>
+        [HttpPost]
+        public void Post([FromBody] string objParam)
+        {
+            var recuperar = objParam;
+        }
+
+        public class Carro
+        {
+            public string Marca { get; set; }
+            public string Montadora { get; set; }
+            public int Ano { get; set; }
+        }
+
+        // PUT api/<CarsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CarsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
